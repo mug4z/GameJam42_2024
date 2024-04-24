f5d1b6110cd347ab01ef87908378afbbff5f7396
SaitoZK <aperron@c1r11s4.42lausanne.ch>
2024-04-24 14:53:45 +0200

    Fixed label bug (it was godots fault btw)

--- Src/Scripts/UpgradeLabel.gd
+++ Src/Scripts/UpgradeLabel.gd
@@ -5,6 +5,8 @@ var base_position
 var speed = 4
 var absolute_time = 0
 
+var test_timer = 0
+
 func _ready():
 	base_position = global_position
 	visible = 0
@@ -14,6 +16,7 @@ func set_base_position(pos):
 	my_set_offset()
 	
 func my_set_offset():
+	set_size(get_size())
 	pivot_offset.x = size.x
 	pivot_offset.y = size.y
 
